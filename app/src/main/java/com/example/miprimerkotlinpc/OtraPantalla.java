afd6f2b493bf359ff0491448789e2ff8e36c7369
romisafuga <68354928+romisafuga@users.noreply.github.com>
2020-11-25 13:36:43 -0600

    Se agrego la pantalla  OtraPantalla en Java

---
+++ app/src/main/java/com/example/miprimerkotlinpc/OtraPantalla.java
@@ -0,0 +1,14 @@
+package com.example.miprimerkotlinpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtraPantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra_pantalla);
+    }
+}
